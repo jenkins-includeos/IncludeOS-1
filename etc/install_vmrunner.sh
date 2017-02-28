0661adcd42cafbb828e0557297a3adf89086addd
Martin Nordsletten <mnordsletten@gmail.com>
2017-02-28 11:14:37 +0100

    vmrunner: Use INCLUDEOS_PREFIX in vmrunner install

--- etc/install_vmrunner.sh
+++ etc/install_vmrunner.sh
@@ -3,19 +3,14 @@
 # Installs the vmrunner python package
 
 INCLUDEOS_SRC=${INCLUDEOS_SRC-$HOME/IncludeOS}
-INCLUDEOS_VMRUNNER_INSTALL=${INCLUDEOS_VMRUNNER_INSTALL-GLOBAL} 
+INCLUDEOS_PREFIX=${INCLUDEOS_PREFIX-/usr/local}
+#INCLUDEOS_VMRUNNER_INSTALL=${INCLUDEOS_VMRUNNER_INSTALL-GLOBAL} 
 
 # Install vmrunner globally if specified
 pushd $INCLUDEOS_SRC/vmrunner
-if [ "$INCLUDEOS_VMRUNNER_INSTALL" = GLOBAL ]; then 
+if [ "$INCLUDEOS_PREFIX" = "/usr/local" ]; then
 	sudo python setup.py install
-elif [ "$INCLUDEOS_VMRUNNER_INSTALL" = DEVELOP ]; then
-	python setup.py develop --user
-elif [ "$INCLUDEOS_VMRUNNER_INSTALL" = LOCAL ]; then
-	python setup.py install --user
-else
-	echo ">>>Error<<< \n Wrong INCLUDEOS_VMRUNNER_INSTALL variable, set to GLOBAL, DEVELOP OR LOCAL"
+else 
+	python setup.py install --prefix=$INCLUDEOS_PREFIX/bin
 fi
 popd
-
-
